ba641c513f9ff85b5f6e5475d8251678569c3d8b
BakGyuri <rbfl9611@gmail.com>
2020-09-28 00:49:14 +0900

    #12

--- account/views.py
+++ account/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.models import User
-from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login, logout as auth_logout, authenticate as auth_authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .forms import CustomUserCreationForm,LoginForm
+
 
 # Create your views here.
+
 def signup(request):
-    form = CustomUserCreationForm(request.POST)
     if request.method=="POST":
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            return render(request,'mainpage/mainpage.html')
-    return render(request, 'account/signup.html',{'form':form})
+            auth_login(request,user)
+            return render(request,'account/loginhome.html')
+        else:
+            return HttpResponse('회원가입이 실패했습니다.')
+    else:
+        form = CustomUserCreationForm()
+        return render(request, 'account/signup.html',{'form':form})
 
 def login(request):
-    return render(request, 'account/login.html')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth_authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return render(request,'account/loginhome.html')
+        else:
+            return HttpResponse('로그인이 실패했습니다.')
+    else:
+        form = LoginForm()
+        return render(request, 'account/login.html',{'form':form})
+        
+    
+
+def logout(request):
+    auth_logout(request)
+    form = LoginForm(request.POST)
+    return render(request,'account/loginhome.html',{'form':form})
+
+def loginhome(request):
+    return render(request,'account/loginhome.html')
