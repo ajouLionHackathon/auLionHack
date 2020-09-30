f91eb32f93dcb87195ebc851d2791f96a6634e2f
ImYurim <ymyu123@ajou.ac.kr>
2020-10-01 01:59:58 +0900

    #29 forms.py 만들기

--- trouble/forms.py
+++ trouble/forms.py
@@ -1,7 +1,15 @@
-from .models import Tr_Comment
+from django import forms
+from .models import Tr_Comment, Trouble
+
 
 class TrCommentForm(forms.ModelForm):
-    
+
     class Meta:
         model = Tr_Comment
         fields = ['body']
+
+
+class TroubleForm(forms.ModelForm):
+    class Meta:
+        model = Trouble
+        fields = ['title', 'body', 'category', 'drawing_Filter']
