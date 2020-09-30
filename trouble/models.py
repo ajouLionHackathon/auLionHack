b230eef9a90d38a8fda9bf906f84a6269a035781
ImYurim <ymyu123@ajou.ac.kr>
2020-10-01 01:52:06 +0900

    #29 Trouble model 수정

--- trouble/models.py
+++ trouble/models.py
@@ -2,19 +2,23 @@ from django.db import models
 from pytz import timezone
 
 # Create your models here.
+
+
 class Trouble(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
-    create_Date=models.DateTimeField()
+    create_Date = models.DateTimeField()
+    drawing_Filter = models.TextField()
+    category = models.TextField()
 
     def __str__(self):
         return self.title
 
 
 class Tr_Comment(models.Model):
-    trouble=models.ForeignKey(Trouble, on_delete=models.CASCADE)
+    trouble = models.ForeignKey(Trouble, on_delete=models.CASCADE)
     body = models.TextField()
-    create_Date=models.DateTimeField(auto_now_add=True)
+    create_Date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.body
